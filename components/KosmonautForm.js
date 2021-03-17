b5c2781f72b18357c2274f284f99ba444dec1aa4
Petr Messner <petr.messner@gmail.com>
2021-03-17 16:21:57 +0100

    add KosmonautForm heading

--- components/KosmonautForm.js
+++ components/KosmonautForm.js
@@ -26,33 +26,36 @@ function KosmonautForm({ onSubmit }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Jméno:
-        <input type="text" name="name" value={inputValues.name} onChange={handleInputChange} autoComplete='off' />
-      </label>
-      <br />
+    <div>
+      <h3>Přidat nového kosmonauta</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Jméno:
+          <input type="text" name="name" value={inputValues.name} onChange={handleInputChange} autoComplete='off' />
+        </label>
+        <br />
 
-      <label>
-        Příjmení:
-        <input type="text" id="surname" name="surname" value={inputValues.surname} onChange={handleInputChange} autoComplete='off' />
-      </label>
-      <br />
+        <label>
+          Příjmení:
+          <input type="text" id="surname" name="surname" value={inputValues.surname} onChange={handleInputChange} autoComplete='off' />
+        </label>
+        <br />
 
-      <label>
-        Datum narození:
-        <input type="date" name="date" value={inputValues.date} onChange={handleInputChange} autoComplete='off' />
-      </label>
-      <br />
+        <label>
+          Datum narození:
+          <input type="date" name="date" value={inputValues.date} onChange={handleInputChange} autoComplete='off' />
+        </label>
+        <br />
 
-      <label>
-        Superschopnost:
-        <input type="text" name="superpower" value={inputValues.superpower} onChange={handleInputChange} autoComplete='off' />
-      </label>
-      <br />
+        <label>
+          Superschopnost:
+          <input type="text" name="superpower" value={inputValues.superpower} onChange={handleInputChange} autoComplete='off' />
+        </label>
+        <br />
 
-      <button type="submit">ULOŽIT</button>
-    </form>
+        <button type="submit">ULOŽIT</button>
+      </form>
+    </div>
   )
 }
 
