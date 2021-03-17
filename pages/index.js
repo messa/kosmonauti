ab4f34e7b78085ae0d38f642c87cbe3dfb57cd96
Petr Messner <petr.messner@gmail.com>
2021-03-17 15:16:49 +0100

    add empty index page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Kosmonauti</title>
+      </Head>
+    </div>
+  )
+}
+
+export default IndexPage
