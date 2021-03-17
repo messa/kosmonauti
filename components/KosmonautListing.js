873f5ea1db624fffdc5de1d1a940d4b4266f44c5
Petr Messner <petr.messner@gmail.com>
2021-03-17 16:37:28 +0100

    store cosmonauts in localStorage

--- components/KosmonautListing.js
+++ components/KosmonautListing.js
@@ -1,3 +1,4 @@
+import { useReducer } from 'react'
 import KosmonautForm from './KosmonautForm'
 import KosmonautTable from './KosmonautTable'
 
@@ -10,11 +11,20 @@ const defaultCosmonauts = [
 ]
 
 function KosmonautListing() {
-  const cosmonauts = defaultCosmonauts
+  const [, forceUpdate ] = useReducer(x => x + 1, 0)
+  // ^^^ https://reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
+  const localStorageKey = 'cosmonauts'
+  const localStorageData = typeof window === 'undefined' ? null : window.localStorage.getItem(localStorageKey)
+  const cosmonauts = localStorageData ? JSON.parse(localStorageData) : defaultCosmonauts
+  const handleKosmnautFormSubmit = (cosmonaut) => {
+    const newCosmonauts = [...cosmonauts, cosmonaut]
+    window.localStorage.setItem(localStorageKey, JSON.stringify(newCosmonauts))
+    forceUpdate()
+  }
   return (
     <div>
       <KosmonautTable items={cosmonauts} />
-      <KosmonautForm onSubmit={v => console.debug(v)} />
+      <KosmonautForm onSubmit={handleKosmnautFormSubmit} />
     </div>
   )
 }
