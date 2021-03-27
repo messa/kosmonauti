bc45389482711cde6598cf0266a0a2fb28359acc
Petr Messner <petr.messner@gmail.com>
2021-03-27 01:34:30 +0100

    make client shared between requests in pages/api/dump.js

--- pages/api/dump.js
+++ pages/api/dump.js
@@ -1,26 +1,29 @@
 const MongoClient = require('mongodb').MongoClient;
 
+async function getClient() {
+  const uri = process.env.MONGODB_URI
+  if (!uri) {
+    throw new Error('MONGODB_URI is not configured')
+  }
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  await client.connect()
+  return client
+}
+
+const clientPromise = getClient()
+
 export default async function handler(req, res) {
   try {
-    const uri = process.env.MONGODB_URI
-    if (!uri) {
-      return res.status(500).json({ error: 'MONGODB_URI is not configured' })
-    }
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-      await client.connect()
-      const db = await client.db()
-      await db.collection('things').insertOne({ hello: 'World!' })
-      const collections = await db.listCollections({}, { nameOnly: true }).toArray()
-      const { databases } = await db.admin().listDatabases()
-      return res.status(200).json({
-        databases,
-        dbStats: await db.stats(),
-        collections: collections
-      })
-    } finally {
-      client.close()
-    }
+    const client = await clientPromise
+    const db = await client.db()
+    await db.collection('things').insertOne({ hello: 'World!' })
+    const collections = await db.listCollections({}, { nameOnly: true }).toArray()
+    const { databases } = await db.admin().listDatabases()
+    return res.status(200).json({
+      databases,
+      dbStats: await db.stats(),
+      collections: collections
+    })
   } catch (err) {
     return res.status(500).json({ error: `Exception caught: ${err}` })
   }
